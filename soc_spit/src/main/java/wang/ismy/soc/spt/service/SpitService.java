6ed8067edaac87ac7682d07dfde6aa551e59c498
MY <715711877@qq.com>
2019-11-12 00:56:31 +0800

    完成吐槽服务吐槽添加

--- soc_spit/src/main/java/wang/ismy/soc/spt/service/SpitService.java
+++ soc_spit/src/main/java/wang/ismy/soc/spt/service/SpitService.java
@@ -12,10 +12,12 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import utils.IdWorker;
 import wang.ismy.soc.spt.dao.SpitDao;
 import wang.ismy.soc.spt.pojo.Spit;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -39,8 +41,29 @@ public class SpitService {
         return spitDao.findById(id).orElse(null);
     }
 
+    /**
+     * 吐槽发布
+     *
+     * @param spit
+     */
     public void save(Spit spit) {
+
         spit.set_id(idWorker.nextId() + "");
+        spit.setPublishtime(new Date());
+        spit.setVisits(0);
+        spit.setShare(0);
+        spit.setThumbup(0);
+        spit.setComment(0);
+        spit.setState("1");
+
+        if (!StringUtils.isEmpty(spit.getParentid())) {
+            // 更新父吐槽回复数
+            Query query = new Query();
+            query.addCriteria(Criteria.where("_id").is(spit.getParentid()));
+            Update update = new Update();
+            update.inc("comment",1);
+            mongoTemplate.updateFirst(query,update,"spit");
+        }
         spitDao.save(spit);
     }
 
@@ -69,14 +92,14 @@ public class SpitService {
         String userId = "0xcaffebabe";
         String key = "thumbup_" + userId + "_" + sid;
         if (redisTemplate.opsForValue().get(key) != null) {
-            return new Result(false,StatusCode.ERROR,"不能重复点赞");
+            return new Result(false, StatusCode.ERROR, "不能重复点赞");
         }
         Query query = new Query();
         query.addCriteria(Criteria.where("_id").is(sid));
         Update update = new Update();
         update.inc("thumbup", 1);
         mongoTemplate.updateFirst(query, update, "spit");
-        redisTemplate.opsForValue().set(key,true);
-        return new Result(true, StatusCode.OK,"点赞成功");
+        redisTemplate.opsForValue().set(key, true);
+        return new Result(true, StatusCode.OK, "点赞成功");
     }
 }
