a3470f7a26639c33ce44bcb0d7df58724f2bf9df
MY <715711877@qq.com>
2019-11-11 14:07:49 +0800

    搭建吐槽微服务

---
+++ soc_spit/src/main/java/wang/ismy/soc/spt/SpitApplication.java
@@ -0,0 +1,22 @@
+package wang.ismy.soc.spt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import utils.IdWorker;
+
+/**
+ * @author MY
+ * @date 2019/11/11 14:05
+ */
+@SpringBootApplication
+public class SpitApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpitApplication.class,args);
+    }
+
+    @Bean
+    public IdWorker idWorker(){
+        return new IdWorker();
+    }
+}
